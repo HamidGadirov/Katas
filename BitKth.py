ecdbe7394fe46ca8ee294c0966971e653657c069
Hamid Gadirov <gadirovh@gmail.com>
2022-11-28 23:34:13 +0100

    bit operations

--- BitKth.py
+++ BitKth.py
@@ -7,6 +7,9 @@ def solution(a, k):
             aRemoved.append(a[i])
     return aRemoved
     
-    # del inputArray[k-1::k]
+    # del inputArray[k-1::k] # k-1 th of every k elements
 
-    
+#  Kill K-th Bit
+#  It's now up to you to write a function that will change the kth bit of n back to 0.
+def solution(n, k):
+    return n & ~(1<<(k-1))
